71984a89b5da4c1f2e70744d740fec20e9c6a2b0
Naveentvm <naveentvm@proton.me>
2023-03-19 15:08:40 +0530

    Added validations using Sweetalert and added copy button and added meta description...

--- script.js
+++ script.js
@@ -3,12 +3,51 @@ const jsonInput = document.querySelector("#jsonInput");
 const convertBtn = document.querySelector("#convertBtn");
 const jsonOutput = document.querySelector("#jsonOutput");
 
+new ClipboardJS(".copy");
+
 convertBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let jsonValue = JSON.parse(jsonInput.value);
-  let mapValue = mapName.value;
+  if (jsonInput.value == "" && mapName.value == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please enter JSON and map name...",
+    });
+  } else if (jsonInput.value == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please enter the JSON...",
+    });
+  } else if (mapName.value == "") {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Please enter the map name...",
+    });
+  } else {
+    try {
+      let jsonValue = JSON.parse(jsonInput.value);
+      console.log("jsonValue:", jsonValue);
+      let mapValue = mapName.value;
 
+      let outputValue = convertJSONToDeluge(jsonValue, mapValue);
+      console.log("outputValue:", outputValue);
+
+      jsonOutput.value = outputValue;
+      jsonOutput.scrollIntoView({ behavior: "smooth" });
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Please check the JSON you entered...",
+      });
+    }
+  }
+});
+
+function convertJSONToDeluge(jsonValue, mapValue) {
   let outputValue = "";
 
   let result = Object.keys(jsonValue).map((key) => [key, jsonValue[key]]);
@@ -20,6 +59,6 @@ convertBtn.addEventListener("click", (e) => {
       outputValue += `${mapValue}.put("${item[0]}", []);\n`;
     }
   });
-  jsonOutput.value = outputValue;
-  jsonOutput.scrollIntoView({ behavior: "smooth" });
-});
+
+  return outputValue;
+}
